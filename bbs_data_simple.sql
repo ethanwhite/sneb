6a2ee998b1f05c4e0457860d8c5c141fda2b6875
Ethan White <ethan@weecology.org>
2014-03-14 09:06:17 -0400

    Add SQL query to extract single table version of BBS
    Based on EcoData Retriever 1.6 install into PostgreSQL

---
+++ bbs_data_simple.sql
@@ -0,0 +1,7 @@
+SELECT bbs.counts.countrynum, bbs.counts.statenum, bbs.counts.route, bbs.routes.lati, bbs.routes.loni, bbs.counts.year, bbs.species.aou, bbs.species.genus, bbs.species.species
+FROM bbs.counts
+JOIN bbs.species ON bbs.counts.aou = bbs.species.aou
+JOIN bbs.weather ON bbs.counts.countrynum = bbs.weather.countrynum AND bbs.counts.statenum = bbs.weather.statenum AND bbs.counts.route = bbs.weather.route AND bbs.counts.rpid = bbs.weather.rpid AND bbs.counts.year = bbs.weather.year
+JOIN bbs.routes ON bbs.counts.countrynum = bbs.routes.countrynum AND bbs.counts.statenum = bbs.routes.statenum AND bbs.counts.route = bbs.routes.route
+WHERE bbs.weather.runtype = 1
+LIMIT 100
