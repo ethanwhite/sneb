8afa03869ede44e86ad90595f533d0d51788fa7d
Ethan White <ethan@weecology.org>
2014-03-14 16:12:38 -0400

    Add an R script for combining site data with environmental data

---
+++ get_env_data.r
@@ -0,0 +1,13 @@
+# Module for extracting environmental data values for biological sites
+# setwd("~/Dropbox/Research/SNEB")
+library(dismo)
+
+bbs_locs = read.csv("./data/bbs_locations.csv")
+coordinates(bbs_locs) = c("long", "lat")
+
+bbs_env = data.frame(bbs_locs)
+
+bioclim_data = getData("worldclim", var = "bio", res = 2.5, path="./data/")
+bioclim_bbs_vals = extract(bioclim_data, bbs_locs)
+bbs_env[4:(4+ncol(bioclim_bbs_vals))] = data.frame(bioclim_bbs_vals)
+write.csv(bbs_env, file="./data/bbs_env_data.csv" )
